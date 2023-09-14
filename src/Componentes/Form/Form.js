cb2ba8443a614f11653fcf3d5ca6ab5159119523
Aris Bello <arisbellomartinez@gmail.com>
2023-09-14 16:38:39 -0500

    Realizado el form

--- src/Componentes/Form/Form.js
+++ src/Componentes/Form/Form.js
@@ -1,8 +1,40 @@
-import React from 'react'
 
-const Form = () => {
+
+
+const Form = ({setEnv,setError,info,setInfo}) => {
+
+  
+
+
+
+  const handleChange = (e)=>{
+    setInfo({
+    ...info,
+      [e.target.name]:e.target.value
+    })
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    if (info.name.length>5 && info.email.endsWith(".com")) {
+      setEnv(true)
+      setError(false)
+    }else{
+      setError(true)
+      setEnv(false)
+    }
+  }
+
+  
+  
   return (
-    <div>Form</div>
+  
+  <form onSubmit={handleSubmit}>
+    <input type="text" name='name' placeholder='Full Name' onChange={handleChange}/>
+    <input type="email" name='email' placeholder='Email' onChange={handleChange}/>
+    <button>Send</button>
+  </form>
+  
   )
 }
 
--- src/Rutas/Contact/Contact.js
+++ src/Rutas/Contact/Contact.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Form from '../../Componentes/Form/Form'
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+  const [info,setInfo]= useState({
+    name:'',
+    email:''
+  })
+  const [env,setEnv]= useState(false)
+  const [error,setError]= useState(false)
+  return (<>
+  
+  <h2>What to know more?</h2>
+  <h3>Send or you question</h3>
+  <Form setEnv={setEnv} setError={setError} info={info} setInfo={setInfo}/>
+  {error && <h3>Por favor verifique su información nuevamente</h3>}
+  {env && <h3>Gracias {info.name}, te contactaremos cuando antes vía</h3>}
+</>)
 }
 
 export default Contact
