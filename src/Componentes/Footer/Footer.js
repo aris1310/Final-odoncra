6eed80d51f7be78a41da81f1e06dbc61b85b09e8
Aris Bello <arisbellomartinez@gmail.com>
2023-09-14 21:11:14 -0500

    Un toque a los estilos en footer

--- src/Componentes/Footer/Footer.js
+++ src/Componentes/Footer/Footer.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import dhLogo from "../../Imagenes/DH.png"
+import styles from "./Footer.module.css"
+import facebook from "../../Imagenes/ico-facebook.png"
+import insta from "../../Imagenes/ico-instagram.png"
+import tikto from "../../Imagenes/ico-tiktok.png"
+import what from "../../Imagenes/ico-whatsapp.png"
+
+
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+  return (<div className={styles.divFooter}>
+
+
+    <div className={styles.divOdo}>Odontologo Incorporation System</div>
+    <div className={styles.divRedes}>
+
+    <img src={dhLogo} alt='' className={styles.dhLogo}/>
+    <div>
+
+    <a href="https://www.facebook.com/"><img src={facebook} className={styles.redes}/></a>
+    <a href="https://www.facebook.com/"><img src={insta} className={styles.redes}/></a>
+    <a href="https://www.facebook.com/"><img src={tikto} className={styles.redes}/></a>
+    <a href="https://www.facebook.com/"><img src={what} className={styles.redes}/></a>
+    </div>
+    
+    </div>
+
+  </div>
+  
+)
 }
 
 export default Footer
